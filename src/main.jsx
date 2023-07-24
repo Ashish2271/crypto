2dffe9c148b0dd3d0ac4526daabbc10e52f3c2b3
Tehnoking <ashispandey138cgmail.com>
2023-07-25 03:04:57 +0530

    56

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {ChakraProvider, theme} from "@chakra-ui/react"
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider theme={theme }>
+    <ColorModeSwitcher/>
     <App />
     </ChakraProvider>
     
